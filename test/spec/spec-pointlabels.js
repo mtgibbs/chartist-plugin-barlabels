7a6c1ed2d4a171d120abd2d243d7e0459d6583a2
Matt Gibbs <matt.gibbs@hpe.com>
2016-01-21 10:41:28 -0500

    Initial Test Commit
    Derived from the tests in chartist-plugin-pointlabels

---
+++ test/spec/spec-pointlabels.js
@@ -0,0 +1,15 @@
+describe('ctBarLabels', function () {
+  'use strict';
+
+  beforeEach(function () {
+
+  });
+
+  afterEach(function () {
+
+  });
+
+  it('should be defined in chartist', function () {
+    expect(window.Chartist.plugins.ctBarLabels).toBeDefined();
+  });
+});
